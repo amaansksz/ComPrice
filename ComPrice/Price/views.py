d25986043631274592d7b87ab3c110cc45739f43
amaansksz <112169159+amaansksz@users.noreply.github.com>
2023-10-31 02:20:13 +0530

    Update views.py

--- ComPrice/Price/views.py
+++ ComPrice/Price/views.py
@@ -26,7 +26,7 @@ def amazon(name):
                 # Initialize the Selenium WebDriver with a custom user agent
                 chrome_options = ChromeOptions()
                 chrome_options.headless = True
-                chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36")  # Replace with your desired user agent
+                chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")  # Replace with your desired user agent
                 webdriver_path = r"C:\\Users\\Amaan Shaikh\\OneDrive - RizviCollegeOfEngineering\\Desktop\\Price Comparison\\Backend\\chromedriver-win64\\chromedriver.exe"
                 service = ChromeService(executable_path=webdriver_path)
                 driver = webdriver.Chrome(service=service, options=chrome_options)
